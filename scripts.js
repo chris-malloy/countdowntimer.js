39c833f8ba1884a6e029992849b780f0207c8e07
chris-malloy <chrismalloymusic@gmail.com>
2017-09-29 22:45:35 -0400

    made a countdown timer in javascript

---
+++ scripts.js
@@ -0,0 +1,37 @@
+// console.log("I AM LOADED!");
+
+// put your functions up at the top
+
+function updateTimer() {
+    // subtract enddate from now
+    var now = new Date();
+    var timeRemaining = endDate.getTime() - now.getTime();
+    // console.log(timeRemaining);
+    var seconds = Math.floor((timeRemaining / 1000) % 60); //mod to get rid of minutes
+    var minutes = Math.floor((timeRemaining / 1000 / 60)) % 60; //mod to get rid of hours
+    var hours = Math.floor((timeRemaining / (1000 * 60 * 60))) % 24; //mod to get rid of days
+    var days = Math.floor((timeRemaining / (1000 * 60 * 60 * 24))) % 7;
+    var weeks = Math.floor(timeRemaining / (1000 * 60 * 60 * 24 * 7));
+
+    // console.log(seconds);
+    // console.log(minutes);
+
+    // document.getElementsByClassName('weeks') // this gives you an array
+    document.getElementsByClassName('weeks')[0].innerHTML = weeks;
+    document.getElementsByClassName('days')[0].innerHTML = days;
+    document.getElementsByClassName('hours')[0].innerHTML = hours;
+    document.getElementsByClassName('minutes')[0].innerHTML = minutes;
+    document.getElementsByClassName('seconds')[0].innerHTML = seconds;
+
+}
+
+var endDate = new Date("January 10, 2018")
+    // console.log(endDate);
+    // setInterval will run the first arg (function) every 1000 milliseconds / 1 second
+    // setInterval comes from the window
+setInterval(function() {
+    // console.log("Tick");
+    // Update the HTML so the right numbers are showing.
+    updateTimer();
+
+}, 1000)
